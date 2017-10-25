322d265cddc2691a89b9d106f9613594d3fb2474
Vinoth Venkatesan <venkat26@purdue.edu>
2017-10-25 17:49:24 -0400

    Added neural net file

--- mnist_data.py
+++ mnist_data.py
@@ -2,43 +2,42 @@ from collections import namedtuple
 from sklearn.datasets import fetch_mldata
 import numpy as np
 
-MNIST_data = namedtuple("MNIST_data", "data, targets")              
+MNIST_data = namedtuple("MNIST_data", "data, target")              
 
 def get_MNIST_data():
     data_dir = "C:\\Users\\vinot\\Desktop"
     mnist = fetch_mldata('MNIST original', data_home = data_dir)
     return mnist
 
-def MNIST_train_test_split_random(mnist):                   
-    train_indices = np.random.randint(0,70000,size=60000)
-    mnist_train = MNIST_data(mnist.data[train_indices,:], mnist.target[train_indices])
-    
-#    test_indices = [i for i in np.arange(0,70000) if i not in train_indices]
-    test_indices = np.setdiff1d(np.arange(0,70000), train_indices)
-    mnist_test = MNIST_data(mnist.data[test_indices], mnist.target[test_indices])
-    return mnist_train, mnist_test
-
 def MNIST_train_test_split(mnist):                   
-    mnist_train = MNIST_data(mnist.data[0:60000,:], mnist.target[0:60000])
+    mnist_train = MNIST_data(mnist.data[:60000,:], mnist.target[:60000])
     mnist_test = MNIST_data(mnist.data[60000:,:], mnist.target[60000:])
-    
-    #mnist_train = {'data': mnist.data[1:60001,:], 'target': mnist.target[1:60001]}
-    #mnist_test = {'data': mnist.data[60000:,:], 'target': mnist.target[60000:]}    
+      
     return mnist_train, mnist_test
 
 def MNIST_train_test_split_k(mnist, k):
     train_indices = np.array([])
     sample_per_class = k/10
 
+    # Trying to get same number of samples per label
     for i in range(10):
-        current_label = np.argwhere(mnist.target == i)
-        train_indices = np.append(train_indices,current_label[:sample_per_class])
-        train_indices = train_indices.astype(int)
-
+        current_label = np.argwhere(mnist.target[:60000] == i)
+        
+        # Checking if there is enough samples
+        if np.size(current_label) < sample_per_class: # Not enough samples for the current label
+            train_indices = np.append(train_indices,current_label)
+        else:    
+            train_indices = np.append(train_indices,current_label[:sample_per_class])
+    
+    # Adding additional samples to get 'k' total samples
+    if np.size(train_indices) < k:
+        remain_data = np.setdiff1d(np.arange(60000),train_indices)
+        train_indices = np.append(train_indices,remain_data[:(k - np.size(train_indices))])
+        
+    train_indices = train_indices.astype(int)
     mnist_train = MNIST_data(mnist.data[train_indices,:], mnist.target[train_indices])
     
-#    for i in xrange(10):
-#        print mnist_train.targets[(i*sample_per_class):(i*sample_per_class + 10)]
+    # Test data stays the same
     test_indices = np.arange(60000,70000)
     mnist_test = MNIST_data(mnist.data[test_indices], mnist.target[test_indices])
     
---
+++ mnist_neural_net.py
@@ -0,0 +1,28 @@
+import mnist_data as mnist
+from sklearn.neural_network import MLPClassifier
+from sklearn import metrics
+import time
+
+mnist_all = mnist.get_MNIST_data()
+
+mnist_train = list()
+mnist_test = list()
+
+for k in xrange(60):  
+    mnist_train_k, mnist_test_k = mnist.MNIST_train_test_split_k(mnist_all, (k + 1)*1000)
+    mnist_train.append(mnist_train_k)
+    mnist_test.append(mnist_test_k)
+
+    print "Number of training samples: {}".format((k+1)*1000)
+
+    t0 = time.clock()
+    mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10, alpha=1e-4,
+                        solver='sgd', tol=1e-4, random_state=1,
+                        learning_rate_init=.1)
+    mlp.fit(mnist_train_k.data, mnist_train_k.target)
+    execTime = time.clock() - t0
+                
+    print "Execution Time for Neural Net: {}".format(execTime)                         
+    
+    y_pred = mlp.predict(mnist_test_k.data)
+    print "Accuracy: {}".format(metrics.accuracy_score(mnist_test_k.target, y_pred))
