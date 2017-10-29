fdf3d13a270a6ca16c4bf945c723c92212f8c8a7
Vinoth_MSRAL <venkat26@purdue.edu>
2017-10-29 18:17:07 -0400

    Fixed indexing errors

--- svm_experiments.py
+++ svm_experiments.py
@@ -9,22 +9,28 @@ import matplotlib.pyplot as plt
 # k- fold cross_validation (10-fold)
 C = np.logspace(-2,2,num=5)
 C = np.append(C,np.array([2,5,20,40,50,60,70,80,90]))
+
+mean_accuracy = np.array([])
+var_accuracy = np.array([])
+k = 0
+accuracy = np.zeros((np.size(C),10))
+
 for i in C:
-    mean_accuracy = np.array([])
-    var_accuracy = np.array([])
     
     # Run cross-validation
-    accuracy = mnist_svm.cross_validation_k_fold(i)       
+    accuracy[k,:] = mnist_svm.cross_validation_k_fold(i)       
     
-    mean_accuracy = np.append(mean_accuracy,np.mean(accuracy))    
-    var_accuracy = np.append(var_accuracy,np.var(accuracy))
+    mean_accuracy = np.append(mean_accuracy,np.mean(accuracy[k,:]))    
+    var_accuracy = np.append(var_accuracy,np.var(accuracy[k,:]))
     
     plt.figure()
-    plt.plot(accuracy)
-    plt.show()
+    plt.plot(accuracy[k,:])
     plt.xlabel('k-fold')
     plt.ylabel('Accuracy')
-    plt.title('Avvuracy vs. CV fold for C = {}'.format(i))
+    plt.title('Accuracy vs. CV fold for C = {}'.format(i))
+    plt.show()
+    
+    k += 1
 # Accuracy/MSE for comparing regularization parameter (in SVM alone, KNN alone , etc,.)
 
 # ROC curves for comparing models (SVM vs. KNN vs. Neural)
