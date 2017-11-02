360675ab56d58ee2ad55a0fa79e956744f00ad24
Vinoth_MSRAL <venkat26@purdue.edu>
2017-11-02 11:19:04 -0400

    Changing params and ranges for experiments

--- mnist_neural_net.py
+++ mnist_neural_net.py
@@ -11,7 +11,9 @@ mnist_all = mnist.get_MNIST_data()
 mnist_train = list()
 mnist_test = list()
 
-# Studying the effect of samples
+"""
+Studying the effect of samples
+"""
 def neural_net_samples():
     accuracy = np.array([])
     for k in xrange(60):  
@@ -46,7 +48,9 @@ def neural_net_samples():
     plt.savefig('accuracy_vs_samples_neural_net_default.png', dpi = 600)
     plt.show()
 
-# optimizing the size of hidden layers
+"""
+Optimizing the size of hidden layers
+"""
 def neural_net_layers_size():
     
     mnist_train_lay, mnist_test_lay = mnist.MNIST_train_test_split(mnist_all)
@@ -55,7 +59,7 @@ def neural_net_layers_size():
     for hidden_layer_sizes in hidden_layer_sizes_list:
         mlp = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, max_iter=400, alpha=1e-4,
                                 solver='sgd', tol=1e-4, random_state=1,
-                                learning_rate_init=.001)
+                                learning_rate_init=.0011, learning_rate='adaptive')
         mlp.fit(mnist_train_lay.data, mnist_train_lay.target)                       
         
         accuracy = mlp.score(mnist_test_lay.data, mnist_test_lay.target)        
@@ -64,7 +68,9 @@ def neural_net_layers_size():
     
         print "Accuracy: {} for Hidden Layer sizes: {}".format(accuracy, hidden_layer_sizes)
 
-# Optimizing neural net params         
+"""
+Optimizing neural net params         
+"""
 def neural_net_opt(alpha, act_fcn, learn_rate, train, test):
     mlp = MLPClassifier(hidden_layer_sizes=(100), max_iter=200, alpha=alpha,
                             solver='sgd', tol=1e-4, random_state=1,
@@ -79,7 +85,9 @@ def neural_net_opt(alpha, act_fcn, learn_rate, train, test):
     
     return accuracy, y_score
     
-# 10-fold CV for param optimization
+"""
+10-fold CV for param optimization
+"""
 def cross_validation_k_fold(alpha = 'default', act_fcn = 'default', learn_rate = 'default'):
     """
     :param alpha: Regularization term
@@ -87,29 +95,33 @@ def cross_validation_k_fold(alpha = 'default', act_fcn = 'default', learn_rate =
     :param learn_rate: Learning rate for the net
     :return: Accuracy score for each fold in the 10-fold CV
     """
+    a = 0; l = 0; ac = 0
     accuracy = np.zeros(10)
     for k in xrange(10):
         
         #        mnist_cv = mnist.MNIST_data(mnist_pca, mnist_all.target[:60000])
         mnist_cv = mnist.MNIST_data(mnist_all.data[:60000,:], mnist_all.target[:60000])
-        
+
         train,test = mnist.MNIST_train_test_split_k(mnist_cv,54000,True,k+1)
-        if alpha == 'default': alpha = 1e-4
-        if learn_rate == 'default': learn_rate = 1e-3
-        if act_fcn == 'default': act_fcn = 'relu'
+        if alpha == 'default': alpha = 1e-4; a = 1
+        if learn_rate == 'default': learn_rate = 1e-3; l = 1
+        if act_fcn == 'default': act_fcn = 'relu'; ac = 1
         
         accuracy[k], y_score = neural_net_opt(alpha, act_fcn, learn_rate, train, test)
         
         # Filename based on param being optimized
-        if alpha == 'default' and act_fcn == 'default':
+        if a == 1 and ac == 1:
             print "Accuracy = {} for k = {} and Learn_rate = {}".format(accuracy[k],k+1,learn_rate)
             filename = 'Neural_Net_linear_ROC_Learn_rate_{}_k_{}.png'.format(learn_rate,k+1)
-        elif alpha == 'default' and learn_rate == 'default':
+        elif a == 1 and l == 1:
             print "Accuracy = {} for k = {} and Activation_fcn = {}".format(accuracy[k],k+1,act_fcn)
             filename = 'Neural_Net_linear_ROC_Activation_fcn_{}_k_{}.png'.format(act_fcn,k+1)
-        else:
+        elif l == 1 and ac == 1:
             print "Accuracy = {} for k = {} and Alpha = {}".format(accuracy[k],k+1,alpha)
             filename = 'Neural_Net_linear_ROC_Alpha_{}_k_{}.png'.format(alpha,k+1)
+        else:
+            print "Accuracy = {} for k = {} and Alpha = {}, Activation_fcn = {}, Learn_rate = {}".format(accuracy[k],k+1,alpha,act_fcn,learn_rate)
+            filename = 'Neural_Net_linear_ROC_Alpha_{}_Act_fcn_{}_Learn_rate_{}_k_{}.png'.format(alpha,act_fcn,learn_rate,k+1)
         
         # Generate ROC plot (all_classes, k-folds)
         title = 'ROC_Neural_Net'
--- neural_net_experiments.py
+++ neural_net_experiments.py
@@ -1,15 +1,22 @@
 import mnist_neural_net
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 
-## Studying the effect of samples
-#mnist_neural_net.neural_net_samples()
-#
-## optimizing the size of hidden layers
-#mnist_neural_net.neural_net_layers_size()
+"""
+Studying the effect of samples
+"""
+mnist_neural_net.neural_net_samples()
 
-# Optimizing the regulairtion term (alpha) 
-alpha = np.logspace(-4,2,7)
+"""
+Optimizing the size of hidden layers
+"""
+mnist_neural_net.neural_net_layers_size()
+
+"""
+Optimizing the regulairtion term (alpha) 
+"""
+alpha = np.logspace(-4,4,9)
 mean_accuracy_alp = np.array([])
 var_accuracy_alp = np.array([])
 k = 0
@@ -31,8 +38,29 @@ for alp in alpha:
     plt.show()   
     k += 1
 
-# Studying effect of activation function 
-act_fcns = ['identity','logistic','tanh', 'relu']
+# Mean_accuracy plots
+plt.figure()
+plt.plot(alpha, mean_accuracy_alp)
+plt.grid(linestyle='--')
+plt.xscale('log')
+plt.gca().set(xlabel=r'$\alpha$ (regularization)', ylabel='Mean Accuracy (10-fold CV)')
+plt.title(r'$\alpha$ (regularization) optimization using the 10-fold CV') 
+plt.savefig('Neural_net_alpha_mean_acc.png', dpi = 600)
+plt.show()  
+
+plt.figure()
+plt.plot(alpha, var_accuracy_alp)
+plt.grid(linestyle='--')
+plt.xscale('log')
+plt.gca().set(xlabel=r'$\alpha$ (regularization)', ylabel='Variance Accuracy (10-fold CV)')
+plt.title(r'$\alpha$ (regularization) optimization using the 10-fold CV') 
+plt.savefig('Neural_net_alpha_var_acc.png', dpi = 600)
+plt.show() 
+
+"""
+Studying effect of activation function 
+"""
+act_fcns = ['logistic','tanh', 'relu']
 mean_accuracy_act_fcn = np.array([])
 var_accuracy_act_fcn = np.array([])
 k = 0
@@ -50,20 +78,47 @@ for func in act_fcns:
     plt.plot(accuracy_act_fcn[k,:])
     plt.xlabel('k-fold')
     plt.ylabel('Accuracy')
-    plt.title('Accuracy vs. CV fold for activation_func = {}'.format(func))
+    plt.title('Accuracy vs. CV fold for activation_func: {}'.format(func))
     plt.show()   
     k += 1
 
-# Optimizing learning rate
-learn_rate = np.arange(0.001, 0.1, 0.001)
+# Mean_accuracy plots
+fig, ax = plt.subplots(1,1)
+ax.set_xticks(np.arange(0,3,1))
+ax.set_xticklabels(act_fcns, rotation=20)
+ax.plot(mean_accuracy_act_fcn)
+ax.grid(linestyle='--')
+ax.set_title('Choosing activation function using the 10-fold CV')
+ax.set_ylabel('Mean accuracy') 
+plt.savefig('Neural_net_act_fcn_mean_acc.png', dpi = 600)
+plt.show()  
+
+# Variance_accuracy plots
+fig, ax = plt.subplots(1,1)
+ax.set_xticks(np.arange(0,3,1))
+ax.set_xticklabels(act_fcns, rotation=20)
+ax.plot(var_accuracy_act_fcn)
+ax.grid(linestyle='--')
+ax.set_title('Choosing activation function using the 10-fold CV')
+ax.set_ylabel('Variance accuracy') 
+plt.savefig('Neural_net_act_fcn_var_acc.png', dpi = 600)
+plt.show()  
+
+"""
+Optimizing learning rate
+"""
+learn_rate = np.arange(0.0001, 0.0021, 0.0001)
 mean_accuracy_learn_rate = np.array([])
 var_accuracy_learn_rate = np.array([])
 k = 0
 accuracy_learn_rate = np.zeros((np.size(learn_rate),10))
+execTime = np.zeros(np.size(learn_rate))
 
 for learn_r in learn_rate:
     # Run cross-validation
+    t0 = time.clock()
     accuracy_learn_rate[k,:] = mnist_neural_net.cross_validation_k_fold(learn_rate=learn_r)       
+    execTime[k] = time.clock() - t0
     
     # Mean and Variance of accuracy metric
     mean_accuracy_learn_rate = np.append(mean_accuracy_learn_rate,np.mean(accuracy_learn_rate[k,:]))    
@@ -76,3 +131,20 @@ for learn_r in learn_rate:
     plt.title('Accuracy vs. CV fold for learning_rate = {}'.format(learn_r))
     plt.show()   
     k += 1
+    
+# Mean_accuracy plots
+plt.figure()
+plt.plot(learn_rate, mean_accuracy_learn_rate)
+plt.grid(linestyle='--')
+plt.gca().set(xlabel=r'Learning rate ($\eta$)', ylabel='Mean Accuracy (10-fold CV)')
+plt.title(r'Learning rate ($\eta$) optimization using the 10-fold CV') 
+plt.savefig('Neural_net_learn_rate_mean_acc.png', dpi = 600)
+plt.show()  
+
+plt.figure()
+plt.plot(learn_rate, var_accuracy_learn_rate)
+plt.grid(linestyle='--')
+plt.gca().set(xlabel=r'Learning rate ($\eta$)', ylabel='Mean Accuracy (10-fold CV)')
+plt.title(r'Learning rate ($\eta$) optimization using the 10-fold CV') 
+plt.savefig('Neural_net_learn_rate_var_acc.png', dpi = 600)
+plt.show() 
