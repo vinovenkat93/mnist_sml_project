c08f17e4e66494d3f66d877d6a47fd53db16f9b3
Vinoth_MSRAL <venkat26@purdue.edu>
2017-11-07 16:35:00 -0500

    Minor edits

--- code/mnist_data.py
+++ code/mnist_data.py
@@ -11,7 +11,7 @@ def get_MNIST_data():
     if platform == "darwin": # Mac OS
         data_dir = "../MNIST_Data"
     elif platform == "win32": # Windows
-        data_dir = "D:\\Vinoth\\Course_work\\CS 578\\Project\\MNIST_Data"
+        data_dir = "..\..\MNIST_Data" 
 
     mnist = fetch_mldata('MNIST original', data_home = data_dir)
     return mnist
